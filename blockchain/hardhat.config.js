3cbf6cc3395b2b7f19fc4b3a1924f10f45d2d9d3
Your Name <user@example.com>
2025-10-22 12:34:58 +0530

    fixed

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     ganache: {
       url: "http://127.0.0.1:7545", // The RPC Server address from Ganache
-      accounts: ['4bff73e77c8d8531d3bee27cf420a8f87b9dfde301a12fe5bab2b4fad3f92d3a'] // The private key from the first account in Ganache
+      accounts: ['020d37762e0ee196cc35bdb381309211724fe17932a7295305ee7a01c2beb2be'] // The private key from the first account in Ganache
     }
   }
 };
